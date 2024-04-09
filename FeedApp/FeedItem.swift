aab220562b181f71b7693ae0ce3b621c69bb58cc
Aram Ispiryan <aispiryan@zerosystems.com>
2024-04-09 13:56:03 +0400

    Initial Commit.

---
+++ FeedApp/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  FeedApp
+//
+//  Created by Aram Ispiryan on 09.04.24.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
