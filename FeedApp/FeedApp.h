04cc1630daea3bb58fc91b9cac65d20d25e3552f
Aram Ispiryan <aispiryan@zerosystems.com>
2024-04-09 13:53:17 +0400

    Initial Commit

---
+++ FeedApp/FeedApp.h
@@ -0,0 +1,18 @@
+//
+//  FeedApp.h
+//  FeedApp
+//
+//  Created by Aram Ispiryan on 09.04.24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FeedApp.
+FOUNDATION_EXPORT double FeedAppVersionNumber;
+
+//! Project version string for FeedApp.
+FOUNDATION_EXPORT const unsigned char FeedAppVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FeedApp/PublicHeader.h>
+
+
