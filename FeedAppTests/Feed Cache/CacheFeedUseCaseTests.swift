850c8d58504d9a75760b61ca9168da50af26f7e3
Aram Ispiryan <ispiryanaram@gmail.com>
2024-04-22 18:45:56 +0400

    Remove redundant test code.

--- FeedAppTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ FeedAppTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -30,7 +30,6 @@ class FeedStore {
     typealias DeletionCompletion = (Error?) -> Void
 
     var deleteCachedFeedCallCount = 0
-    var insertCallCount = 0
     var insertions = [(items: [FeedItem], timestamp: Date)]()
 
     private var deletionCompletions = [DeletionCompletion]()
@@ -49,7 +48,6 @@ class FeedStore {
     }
 
     func insert(_ items: [FeedItem], timestamp: Date) {
-        insertCallCount += 1
         insertions.append((items, timestamp))
     }
 }
@@ -79,20 +77,10 @@ class CacheFeedUseCaseTests: XCTestCase {
         sut.save(items)
         store.completeDeletion(with: deletionError)
 
-        XCTAssertEqual(store.insertCallCount, 0)
+        XCTAssertEqual(store.insertions.count, 0)
     }
 
-    func test_save_requestsNewCacheInsertionOnSuccessfulDeletion  () {
-        let (sut, store) = makeSUT()
-        let items = [uniqueItem(), uniqueItem()]
-
-        sut.save(items)
-        store.completeDeletionSuccessfully()
-
-        XCTAssertEqual(store.insertCallCount, 1)
-    }
-
-    func test_save_requestsNewCacheInsertionWithTimestampOnSuccessfulDeletion  () {
+    func test_save_requestsNewCacheInsertionWithTimestampOnSuccessfulDeletion () {
         let timestamp = Date()
         let (sut, store) = makeSUT(currentDate: { timestamp })
         let items = [uniqueItem(), uniqueItem()]
@@ -100,7 +88,9 @@ class CacheFeedUseCaseTests: XCTestCase {
         sut.save(items)
         store.completeDeletionSuccessfully()
 
-        XCTAssertEqual(store.insertCallCount, 1)
+        XCTAssertEqual(store.insertions.count, 1)
+        XCTAssertEqual(store.insertions.first?.items, items)
+        XCTAssertEqual(store.insertions.first?.timestamp, timestamp)
     }
 
     // MARK: Helpers
