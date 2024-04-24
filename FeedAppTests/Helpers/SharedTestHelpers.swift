8a76b63d0112a9ede64ab45a0d208471e8cff7e3
Aram Ispiryan <ispiryanaram@gmail.com>
2024-04-24 21:15:25 +0400

    Extract helpers into a shared scop/

--- FeedAppTests/Helpers/SharedTestHelpers.swift
+++ FeedAppTests/Helpers/SharedTestHelpers.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+func anyURL() -> URL {
+    return URL(string: "http://any-url.com")!
+}
+
+
+func anyNSError() -> NSError {
+    return NSError(domain: "any error", code: 0)
+}
