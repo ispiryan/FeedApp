729b703c0c8ee13afd6d577da202ef0e707f5c3b
Aram Ispiryan <ispiryanaram@gmail.com>
2024-05-03 15:45:25 +0400

    CoreDataFeedStore.retrieve() delivers empty on empty cache

---
+++ FeedApp/Feed Cashe/CoreDataFeedStore.swift
@@ -0,0 +1,8 @@
+//
+//  CoreDataFeedStore.swift
+//  FeedApp
+//
+//  Created by Aram Ispiryan on 03.05.24.
+//
+
+import Foundation
---
+++ FeedAppTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -0,0 +1,8 @@
+//
+//  CoreDataFeedStoreTests.swift
+//  FeedAppTests
+//
+//  Created by Aram Ispiryan on 03.05.24.
+//
+
+import Foundation
