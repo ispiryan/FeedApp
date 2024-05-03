142f3f917294559aaf9e91f9952fb77f303d52ce
Aram Ispiryan <ispiryanaram@gmail.com>
2024-05-03 15:45:35 +0400

    CoreDataFeedStore.retrieve() delivers empty on empty cache

--- FeedAppTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ FeedAppTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -5,4 +5,64 @@
 //  Created by Aram Ispiryan on 03.05.24.
 //
 
-import Foundation
+import XCTest
+import FeedApp
+
+class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
+
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+        let sut = makeSUT()
+
+        assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
+    }
+
+    func test_retrieve_hasNoSideEffectsOnEmptyCache() {
+
+    }
+
+    func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
+
+    }
+
+    func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
+
+    }
+
+    func test_insert_deliversNoErrorOnEmptyCache() {
+
+    }
+
+    func test_insert_deliversNoErrorOnNonEmptyCache() {
+
+    }
+
+    func test_insert_overridesPreviouslyInsertedCacheValues() {
+
+    }
+
+    func test_delete_deliversNoErrorOnEmptyCache() {
+
+    }
+
+    func test_delete_hasNoSideEffectsOnEmptyCache() {
+
+    }
+
+    func test_delete_deliversNoErrorOnNonEmptyCache() {
+
+    }
+
+    func test_delete_emptiesPreviouslyInsertedCache() {
+
+    }
+
+    func test_storeSideEffects_runSerially() {
+
+    }
+
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> FeedStore {
+        let sut = CoreDataFeedStore()
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
+}
