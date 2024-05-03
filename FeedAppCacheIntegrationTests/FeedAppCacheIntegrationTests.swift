cc9ef0d20a3f947c242d4df07811992975fc91cf
Aram Ispiryan <ispiryanaram@gmail.com>
2024-05-03 19:13:04 +0400

    AddFeedAppCacheIntegrationTests target to separate the potentially-slower cache integration tests from the fast unit/isolated tests

---
+++ FeedAppCacheIntegrationTests/FeedAppCacheIntegrationTests.swift
@@ -0,0 +1,13 @@
+//
+//  FeedAppCacheIntegrationTests.swift
+//  FeedAppCacheIntegrationTests
+//
+//  Created by Aram Ispiryan on 03.05.24.
+//
+
+import XCTest
+
+final class FeedAppCacheIntegrationTests: XCTestCase {
+
+
+}
