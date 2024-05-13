e7ee23c88e4defcc5d00f5b03ce0545a2ea377f0
Aram Ispiryan <ispiryanaram@gmail.com>
2024-05-13 19:03:27 +0400

    Add TDD UIViewController with fake RefreshControl.

---
+++ FeedAppiOS/FeedViewController.swift
@@ -0,0 +1,8 @@
+//
+//  FeedViewController.swift
+//  FeedAppiOS
+//
+//  Created by Aram Ispiryan on 13.05.24.
+//
+
+import Foundation
---
+++ FeedAppiOSTests/FeedViewControllerTests.swift
@@ -0,0 +1,20 @@
+//
+//  FeedViewControllerTests.swift
+//  FeedAppiOSTests
+//
+//  Created by Aram Ispiryan on 13.05.24.
+//
+
+import XCTest
+
+final class FeedViewControllerTests: XCTestCase {
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+}
