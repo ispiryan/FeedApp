2e0d77d3bfbffb5201148a0bfe95a6e79cb88813
Aram Ispiryan <ispiryanaram@gmail.com>
2024-05-14 16:56:05 +0400

    Render loaded feed on successful load completion.

---
+++ FeedAppiOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  FeedAppiOS
+//
+//  Created by Aram Ispiryan on 14.05.24.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
