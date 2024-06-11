2c68c97d14500670238c83adc3078ab8f90b1bb7
ispiryanaram <ispiryanaram@gmail.com>
2024-06-11 23:10:28 +0400

    Add Localization files for multiple anguages and cover with tests.

--- FeedAppiOSTest/Feed UI/Controllers/Helpers/FeedUIIntegrationTests +Assertions.swift
+++ FeedAppiOSTest/Feed UI/Controllers/Helpers/FeedUIIntegrationTests +Assertions.swift
@@ -1,5 +1,5 @@
 //
-//  FeedViewControllerTests+Assertions.swift
+//  FeedUIIntegrationTests+Assertions.swift
 //  FeedAppiOSTests
 //
 //  Created by Aram Ispiryan on 15.05.24.
@@ -9,7 +9,7 @@ import XCTest
 import FeedApp
 import FeedAppiOS
 
-extension FeedViewControllerTests {
+extension FeedUIIntegrationTests {
 
     func assertThat(_ sut: FeedViewController, isRendering feed: [FeedImage], file: StaticString = #file, line: UInt = #line) {
         guard sut.numberOfRenderedFeedImageViews() == feed.count else {
