a5a4b3d2c8a5249c229aa5c734c2cfc352b0aba1
ispiryanaram <ispiryanaram@gmail.com>
2024-06-13 22:19:04 +0400

    Implemented threading strategy to prevent UI updates outside the main queue using the Decorator pattern. This way, the UI is agnostic of threading logic/details.
    Threading is dealt with in the Composition layer.

--- FeedAppiOS/Feed UI/Composers/FeedViewAdapter.swift
+++ FeedAppiOS/Feed UI/Composers/FeedViewAdapter.swift
@@ -5,4 +5,28 @@
 //  Created by Aram Ispiryan on 13.06.24.
 //
 
-import Foundation
+import UIKit
+import FeedApp
+
+final class FeedViewAdapter: FeedView {
+    private weak var controller: FeedViewController?
+    private let imageLoader: FeedImageDataLoader
+
+    init(controller: FeedViewController, imageLoader: FeedImageDataLoader) {
+        self.controller = controller
+        self.imageLoader = imageLoader
+    }
+
+    func display(_ viewModel: FeedViewModel) {
+        controller?.tableModel = viewModel.feed.map { model in
+            let adapter = FeedImageDataLoaderPresentationAdapter<WeakRefVirtualProxy<FeedImageCellController>, UIImage>(model: model, imageLoader: imageLoader)
+            let view = FeedImageCellController(delegate: adapter)
+
+            adapter.presenter = FeedImagePresenter(
+                view: WeakRefVirtualProxy(view),
+                imageTransformer: UIImage.init)
+
+            return view
+        }
+    }
+}
--- FeedAppiOS/Feed UI/Composers/WeakRefVirtualProxy.swift
+++ FeedAppiOS/Feed UI/Composers/WeakRefVirtualProxy.swift
@@ -5,4 +5,24 @@
 //  Created by Aram Ispiryan on 13.06.24.
 //
 
-import Foundation
+import UIKit
+
+final class WeakRefVirtualProxy<T: AnyObject> {
+    private weak var object: T?
+
+    init(_ object: T) {
+        self.object = object
+    }
+}
+
+extension WeakRefVirtualProxy: FeedLoadingView where T: FeedLoadingView {
+    func display(_ viewModel: FeedLoadingViewModel) {
+        object?.display(viewModel)
+    }
+}
+
+extension WeakRefVirtualProxy: FeedImageView where T: FeedImageView, T.Image == UIImage {
+    func display(_ model: FeedImageViewModel<UIImage>) {
+        object?.display(model)
+    }
+}
