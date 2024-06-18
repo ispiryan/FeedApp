4e63f41d72810ae7aed542356b0eed949175b88a
ispiryanaram <ispiryanaram@gmail.com>
2024-06-18 21:45:27 +0400

    Move ErrorView to separate file

---
+++ FeedAppiOS/Feed UI/Views/ErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorView.swift
+//  FeedAppiOS
+//
+//  Created by Aram Ispiryan on 18.06.24.
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}
